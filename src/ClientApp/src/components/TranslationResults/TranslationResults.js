e99051979c07f724920039a1e98017821bd698db
Seth Reid <seth.reid@xero.com>
2021-04-11 12:11:18 +1200

    fix build

--- src/ClientApp/src/components/TranslationResults/TranslationResults.js
+++ src/ClientApp/src/components/TranslationResults/TranslationResults.js
@@ -1,23 +1,26 @@
 import { useEffect, useState } from "react";
 
 export const TranslationResults = ({ results }) => {
-  const [textToDisplay, setTextToDisplay] = useState("");
+  const [textToDisplay, setTextToDisplay] = useState([]);
   useEffect(() => {
     const playAndDisplayAudio = async () => {
       for (let index = 0; index < results.length; index++) {
         const translation = results[index];
         var audio = new Audio(translation.ttsAudioUrl);
         audio.type = "audio/wav";
-        setTimeout(() => {
-          audio.play();
-          setTextToDisplay(`${textToDisplay} \n ${translation.translatedText}`);
-        }, 4000);
+        audio.play();
+        setTextToDisplay([...textToDisplay, translation.translatedText]);
       }
     };
     if (results && results.length > 0) {
-      debugger;
       playAndDisplayAudio();
     }
-  }, [results]);
-  return <>{textToDisplay}</>;
+  }, [results, textToDisplay]);
+  return (
+    <>
+      {textToDisplay.map((text) => (
+        <p>{text}</p>
+      ))}
+    </>
+  );
 };
--- src/ClientApp/src/utility/audio.js
+++ src/ClientApp/src/utility/audio.js
@@ -1,33 +1,3 @@
-const getAudioFileLength = async () =>
-  new Promise((resolve, reject) => {
-    var audio = document.createElement("audio");
-
-    // Add a change event listener to the file input
-    document.getElementById("fileinput").addEventListener(
-      "change",
-      function (event) {
-        var target = event.currentTarget;
-        var file = target.files[0];
-        let reader = new FileReader();
-
-        if (target.files && file) {
-          reader = new FileReader();
-
-          reader.onload = function (e) {
-            audio.src = e.target.result;
-            audio.addEventListener(
-              "loadedmetadata",
-              function () {
-                var duration = audio.duration;
-                resolve(audio);
-              },
-              false
-            );
-          };
-
-          reader.readAsDataURL(file);
-        }
-      },
-      false
-    );
-  });
+export const getAudioFileLength = async () => {
+  // TODO
+};
